6d4cfac3b202c577fc7f60d26f1bf8dc9dfa1259
Matthew H <matej.husak@icloud.com>
2021-10-23 10:52:32 +0200

    removed dependency, fixed code

--- generator.js
+++ generator.js
@@ -2,15 +2,13 @@ const puppeteer = require('puppeteer')
 const handlebars = require('handlebars')
 const fs = require("fs")
 
-class Generator {
-
-    invoiceItems;
-    template;
-    renderedTemplate;
-    data;
-
-    // default settings for PDF
-    pdfOptions = {
+// new class declaration that is suitable for webpack compiler
+function Generator(template, data, invoiceItems) {
+    this.invoiceItems = invoiceItems
+    this.template = template
+    this.renderedTemplate = null
+    this.data = data
+    this.pdfOptions = {
         format: 'A4',
         headerTemplate: "<p></p>",
         footerTemplate: "<p></p>",
@@ -22,69 +20,34 @@ class Generator {
         printBackground: true,
         path: 'invoice.pdf'
     };
+}
 
-    // main constructor
-    constructor (template, data, invoiceItems, pdfOptions = {}) {
-        this.template = template;
-        this.data = data;
-        this.invoiceItems = invoiceItems;
+Generator.prototype.getTemplate = function () {
+    return this.template
+}
 
-        // if pdf options are not being set, set the default options
-        if (pdfOptions.length > 0)
-            this.pdfOptions = pdfOptions
-    }
+Generator.prototype.getData = function () {
+    return this.data
+}
 
-    // getter for template
-    get template () {
-        return this.template;
-    }
+Generator.prototype.renderTemplate = function () {
+    let templateHandlebars = handlebars.compile(this.template)
+    this.renderedTemplate = templateHandlebars(this.data)
+}
 
-    get renderedTemplate () {
-        return this.renderTemplate();
-    }
+Generator.prototype.generatePDF = async function () {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto(`data:text/html;charset=UTF-8,${encodeURIComponent(this.renderedTemplate)}`, {
+        waitUntil: 'networkidle0'
+    });
+    await page.pdf(this.pdfOptions);
+    await browser.close();
 
-    // getter for options
-    get data () {
-        return this.data;
-    }
-
-    // getter for invoice items
-    get invoiceItems () {
-        return this.invoiceItems;
-    }
-
-    // method that renders template
-    renderTemplate () {
-        let templateHandlebars = handlebars.compile(this.template)
-        this.renderedTemplate = templateHandlebars(this.data)
-    }
-
-    // method that fills the template
-    // OBSOLETE
-    fillTemplate () {
-        // custom system
-        for (const key in this.data) {
-            this.template = this.template.replace(key, this.data[key]);
-        }
-    }
-    // OBSOLETE
-
-    // method that generate a PDF from a html template
-    // returns PDF path
-    async generatePDF (returnBuffer = false) {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox'],
-            headless: true
-        });
-        const page = await browser.newPage();
-        await page.goto(`data:text/html;charset=UTF-8,${encodeURIComponent(this.renderedTemplate)}`, {
-            waitUntil: 'networkidle0'
-        });
-        await page.pdf(this.pdfOptions);
-        await browser.close();
-
-        return this.pdfOptions.path
-    }
+    return this.pdfOptions.path
 }
 
 module.exports = Generator;
